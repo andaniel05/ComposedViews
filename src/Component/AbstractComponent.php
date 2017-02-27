a7f8758e26f5903542c591ef96fe78001a9dc349
Andy D. Navarro Taño <andaniel05@gmail.com>
2017-02-27 15:27:00 -0400

    temp

--- src/Component/AbstractComponent.php
+++ src/Component/AbstractComponent.php
@@ -3,10 +3,14 @@
 namespace PlatformPHP\ComposedViews\Component;
 
 use PlatformPHP\ComposedViews\RenderInterface;
+use PlatformPHP\ComposedViews\Traits\PrintTrait;
 
 abstract class AbstractComponent implements RenderInterface
 {
+    use PrintTrait;
+
     protected $id;
+    protected $parent;
 
     public function __construct(string $id)
     {
@@ -16,5 +20,15 @@ abstract class AbstractComponent implements RenderInterface
     public function getId() : string
     {
         return $this->id;
+    }
+
+    public function setParent(AbstractComponent $parent)
+    {
+        $this->parent = $parent;
+    }
+
+    public function getParent() : ?AbstractComponent
+    {
+        return $this->parent;
     }
 }
--- tests/Component/AbstractComponentTest.php
+++ tests/Component/AbstractComponentTest.php
@@ -6,6 +6,13 @@ use PlatformPHP\ComposedViews\Component\AbstractComponent;
 
 class AbstractComponentTest extends \PHPUnit_Framework_TestCase
 {
+    public function setUp()
+    {
+        $this->component = $this->getMockBuilder(AbstractComponent::class)
+            ->setConstructorArgs(['id'])
+            ->getMockForAbstractClass();
+    }
+
     public function provider1()
     {
         return [
@@ -24,5 +31,49 @@ class AbstractComponentTest extends \PHPUnit_Framework_TestCase
             ->getMockForAbstractClass();
 
         $this->assertEquals($id, $component->getId());
+    }
+
+    public function testGetParent_ReturnNullInDefault()
+    {
+        $this->assertNull($this->component->getParent());
+    }
+
+    public function testGetParent_ReturnInsertedValueBySetParent()
+    {
+        $parent = $this->getMockBuilder(AbstractComponent::class)
+            ->disableOriginalConstructor()
+            ->getMockForAbstractClass();
+
+        $child = clone $parent;
+        $child->setParent($parent);
+
+        $this->assertSame($parent, $child->getParent());
+    }
+
+    public function provider2()
+    {
+        return [
+            ['result1'], ['result2'],
+        ];
+    }
+
+    /**
+     * @dataProvider provider2
+     */
+    public function testPrint_PrintResultOfRenderMethod($renderResult)
+    {
+        $component = $this->getMockBuilder(AbstractComponent::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['render'])
+            ->getMockForAbstractClass();
+        $component->expects($this->once())
+            ->method('render')
+            ->willReturn($renderResult);
+
+        ob_start();
+        $component->print();
+        $result = ob_get_clean();
+
+        $this->assertEquals($renderResult, $result);
     }
 }
