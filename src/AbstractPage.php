62453b61948d33d80b0db01fc8057152ed93e28a
Andy D. Navarro Taño <andaniel05@gmail.com>
2017-02-28 12:48:50 -0400

    Temp

--- src/AbstractPage.php
+++ src/AbstractPage.php
@@ -4,4 +4,8 @@ namespace PlatformPHP\ComposedViews;
 
 class AbstractPage
 {
+    public function getPageAssets() : ?AssetCollection
+    {
+        return null;
+    }
 }
--- tests/AbstractPageTest.php
+++ tests/AbstractPageTest.php
@@ -5,9 +5,15 @@ namespace PlatformPHP\ComposedViews\Tests;
 use PHPUnit\Framework\TestCase;
 use PlatformPHP\ComposedViews\AbstractPage;
 
-/**
- * @requires extension none
- */
 class AbstractPageTest extends TestCase
 {
+    public function setUp()
+    {
+        $this->page = $this->getMockForAbstractClass(AbstractPage::class);
+    }
+
+    public function testPageAssets_ReturnNullByDefault()
+    {
+        $this->assertNull($this->page->getPageAssets());
+    }
 }
