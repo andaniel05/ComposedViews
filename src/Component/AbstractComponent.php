3f70c26e4fa0b3686ec1a8befe8da88e81e71eee
Andy D. Navarro Taño <andaniel05@gmail.com>
2017-08-23 06:30:44 -0400

    Ajuste en los componentes

--- src/Component/AbstractComponent.php
+++ src/Component/AbstractComponent.php
@@ -6,15 +6,15 @@ use PlatformPHP\ComposedViews\{AbstractPage, HtmlInterface};
 use PlatformPHP\ComposedViews\Asset\AssetsTrait;
 use PlatformPHP\ComposedViews\Traits\{PageTrait, CloningTrait};
 
-abstract class AbstractComponent implements HtmlInterface, ComponentContainerInterface
+abstract class AbstractComponent implements HtmlInterface
 {
     use AssetsTrait;
     use PageTrait;
     use CloningTrait;
-    use ComponentContainerTrait;
 
     protected $id;
     protected $parent;
+    protected $components = [];
 
     public function __construct(string $id)
     {
@@ -56,5 +56,80 @@ HTML;
         }
 
         return $result;
+    }
+
+    public function getAllComponents(): array
+    {
+        return $this->components;
+    }
+
+    private function findOne(array $components, string $id): ?AbstractComponent
+    {
+        foreach ($components as $component) {
+            if ($id == $component->getId()) {
+                return $component;
+            } elseif ($component instanceOf AbstractComponent) {
+                $component = $this->findOne($component->getAllComponents(), $id);
+                if ($component) {
+                    return $component;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public function getComponent(string $id): ?AbstractComponent
+    {
+        $idList = preg_split('/\s+/', $id);
+
+        if (1 == count($idList)) {
+            return $this->findOne($this->getAllComponents(), $id);
+        }
+
+        $hash = array_fill_keys($idList, null);
+
+        $container = $this;
+        for ($i = 0; $i < count($idList); $i++) {
+
+            $componentId = $idList[$i];
+            $component = $container->getComponent($componentId);
+
+            if ($component) {
+                $hash[$componentId] = $component;
+                $container = $component;
+            } else {
+                break;
+            }
+        }
+
+        return array_pop($hash);
+    }
+
+    public function addComponent(AbstractComponent $component)
+    {
+        $this->components[$component->getId()] = $component;
+
+        if ($this instanceOf AbstractComponent) {
+            $component->setParent($this);
+        }
+    }
+
+    public function dropComponent(string $id, bool $notifyChild = true)
+    {
+        $component = $this->components[$id] ?? null;
+        if ($component) {
+
+            if ($notifyChild) {
+                $component->setParent(null);
+            }
+
+            unset($this->components[$id]);
+        }
+    }
+
+    public function existsComponent(string $id): bool
+    {
+        return isset($this->components[$id]);
     }
 }
--- tests/Component/AbstractComponentTest.php
+++ tests/Component/AbstractComponentTest.php
@@ -4,13 +4,10 @@ namespace PlatformPHP\ComposedViews\Tests;
 
 use PlatformPHP\ComposedViews\AbstractPage;
 use PlatformPHP\ComposedViews\Component\AbstractComponent;
-use PlatformPHP\ComposedViews\Tests\TestCase;
-use PlatformPHP\ComposedViews\Tests\Asset\AssetsTraitTests;
+use PHPUnit\Framework\TestCase;
 
 class AbstractComponentTest extends TestCase
 {
-    use AssetsTraitTests;
-
     public function setUp()
     {
         $this->component = $this->getMockBuilder(AbstractComponent::class)
@@ -128,5 +125,259 @@ class AbstractComponentTest extends TestCase
 HTML;
 
         $this->assertXmlStringEqualsXmlString($expected, $component2->childrenHtml());
+    }
+
+
+
+
+
+    public function getComponentMock(string $id = 'component')
+    {
+        $component = $this->getMockForAbstractClass(AbstractComponent::class, [$id]);
+
+        return $component;
+    }
+
+    public function testGetAllComponentsReturnAnEmptyArrayByDefault()
+    {
+        $component = $this->getComponentMock();
+
+        $this->assertEquals([], $component->getAllComponents());
+    }
+
+    public function testGetComponentReturnNullIfComponentNotExists()
+    {
+        $component = $this->getComponentMock();
+
+        $this->assertNull($component->getComponent('component1'));
+    }
+
+    public function insertTwoComponents()
+    {
+        $this->component1 = $this->createMock(AbstractComponent::class);
+        $this->component1->method('getId')->willReturn('component1');
+        $this->component2 = $this->createMock(AbstractComponent::class);
+        $this->component2->method('getId')->willReturn('component2');
+
+        $this->container = $this->getComponentMock();
+        $this->container->addComponent($this->component1);
+        $this->container->addComponent($this->component2);
+    }
+
+    public function testGetComponentReturnTheComponentIfExists()
+    {
+        $this->insertTwoComponents();
+
+        $this->assertSame(
+            $this->component1,
+            $this->container->getComponent('component1')
+        );
+    }
+
+    public function testGetAllComponentsReturnAnArrayWithAllInsertedComponents()
+    {
+        $this->insertTwoComponents();
+
+        $expected = [
+            'component1' => $this->component1,
+            'component2' => $this->component2,
+        ];
+
+        $this->assertEquals($expected, $this->container->getAllComponents());
+    }
+
+    public function testDropComponentRemoveTheComponentWhenExists()
+    {
+        $this->insertTwoComponents();
+
+        $this->container->dropComponent('component2');
+
+        $this->assertEquals(
+            ['component1' => $this->component1],
+            $this->container->getAllComponents()
+        );
+    }
+
+    public function testExistsComponentReturnFalseWhenComponentNotExists()
+    {
+        $this->insertTwoComponents();
+
+        $this->assertFalse($this->container->existsComponent('component5'));
+    }
+
+    public function testExistsComponentReturnTrueWhenComponentExists()
+    {
+        $this->insertTwoComponents();
+
+        $this->assertTrue($this->container->existsComponent('component1'));
+        $this->assertTrue($this->container->existsComponent('component2'));
+    }
+
+    public function initializeNestedComponents()
+    {
+        $component = $this->getComponentMock();
+
+        $component1 = $this->getMockBuilder(AbstractComponent::class)
+            ->setConstructorArgs(['component1'])
+            ->getMockForAbstractClass();
+
+        $component2 = $this->getMockBuilder(AbstractComponent::class)
+            ->setConstructorArgs(['component2'])
+            ->getMockForAbstractClass();
+
+        $component3 = $this->getMockBuilder(AbstractComponent::class)
+            ->setConstructorArgs(['component3'])
+            ->getMockForAbstractClass();
+
+        $component4 = $this->getMockBuilder(AbstractComponent::class)
+            ->setConstructorArgs(['component4'])
+            ->getMockForAbstractClass();
+
+        $component5 = $this->getMockBuilder(AbstractComponent::class)
+            ->setConstructorArgs(['component5'])
+            ->getMockForAbstractClass();
+
+        $component1->addComponent($component2);
+        $component2->addComponent($component3);
+        $component4->addComponent($component5);
+
+        $component->addComponent($component1);
+        $component->addComponent($component4);
+
+        $this->container  = $component;
+        $this->component1 = $component1;
+        $this->component2 = $component2;
+        $this->component3 = $component3;
+        $this->component4 = $component4;
+        $this->component5 = $component5;
+    }
+
+    public function testGetComponentSearchTheComponentInAllTheTree()
+    {
+        $this->initializeNestedComponents();
+
+        $this->assertSame(
+            $this->component2,
+            $this->container->getComponent('component2')
+        );
+        $this->assertSame(
+            $this->component3,
+            $this->container->getComponent('component3')
+        );
+        $this->assertSame(
+            $this->component5,
+            $this->container->getComponent('component5')
+        );
+    }
+
+    public function testGetComponentWhenIdIsComplex1()
+    {
+        $this->initializeNestedComponents();
+
+        $this->assertSame(
+            $this->component5,
+            $this->container->getComponent('component4 component5')
+        );
+    }
+
+    public function testGetComponentWhenIdIsComplex2()
+    {
+        $this->initializeNestedComponents();
+
+        $this->assertSame(
+            $this->component3,
+            $this->container->getComponent('component1 component3')
+        );
+    }
+
+    public function testGetComponentWhenIdIsComplex3()
+    {
+        $this->initializeNestedComponents();
+
+        $this->assertSame(
+            $this->component3,
+            $this->container->getComponent('component1 component2 component3')
+        );
+    }
+
+    public function testGetComponentWhenIdIsComplex4()
+    {
+        $this->initializeNestedComponents();
+
+        $this->assertSame(
+            $this->component3,
+            $this->container->getComponent('component2 component3')
+        );
+    }
+
+    public function testGetComponentWhenIdIsComplex5()
+    {
+        $this->initializeNestedComponents();
+
+        $this->assertNull(
+            $this->container->getComponent('component1 component5')
+        );
+    }
+
+    public function insertChildComponent()
+    {
+        $this->container = $this->getComponentMock();
+        $this->child = $this->getMockBuilder(AbstractComponent::class)
+            ->setConstructorArgs(['child'])
+            ->getMockForAbstractClass();
+
+        $this->container->addComponent($this->child);
+    }
+
+    public function testAddComponentRegisterToItSelfAsParentInTheChild()
+    {
+        if ($this->getTestClass() == ComponentContainerTrait::class) {
+            $this->markTestSkipped();
+        }
+
+        $this->insertChildComponent();
+
+        $this->assertSame($this->container, $this->child->getParent());
+    }
+
+    public function testDropComponentSetNullAsParentInTheChild()
+    {
+        $this->insertChildComponent();
+
+        $this->container->dropComponent('child');
+
+        $this->assertNull($this->child->getParent());
+    }
+
+    public function initialization1()
+    {
+        $this->child = $this->getMockBuilder(AbstractComponent::class)
+            ->setConstructorArgs(['child'])
+            ->setMethods(['setParent'])
+            ->getMockForAbstractClass();
+
+        $this->container = $this->getComponentMock();
+        $this->container->addComponent($this->child);
+    }
+
+    public function testDropComponentInvokeSetParentWithNullInTheChild()
+    {
+        $this->initialization1();
+
+        $this->child->expects($this->once())
+            ->method('setParent')
+            ->with($this->equalTo(null));
+
+        $this->container->dropComponent('child');
+    }
+
+    public function testDropComponentNotInvokeToSetParentWithNullInTheChildWhenNotifyChildArgumentIsFalse()
+    {
+        $this->initialization1();
+
+        $this->child->expects($this->exactly(0))
+            ->method('setParent');
+
+        $this->container->dropComponent('child', false);
     }
 }
