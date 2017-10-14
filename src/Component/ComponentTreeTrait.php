eb26bfc4f477827a99c1f62fdb07e74e8cd34a16
Andy D. Navarro Taño <andaniel05@gmail.com>
2017-10-14 10:17:02 -0400

    Tem

---
+++ src/Component/ComponentTreeTrait.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Andaniel05\ComposedViews\Component;
+
+trait ComponentTreeTrait
+{
+    protected $components = [];
+
+    public function traverse(): iterable
+    {
+        $generator = function (array $components) use (&$generator) {
+            foreach ($components as $component) {
+                yield $component;
+                yield from $generator($component->getChildren());
+            }
+        };
+
+        return $generator($this->components);
+    }
+}
---
+++ tests/Component/ComponentTreeTraitTest.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Andaniel05\ComposedViews\Tests\Component;
+
+use Andaniel05\ComposedViews\Component\{AbstractComponent, ComponentTreeTrait};
+use PHPUnit\Framework\TestCase;
+
+class ComponentTreeTraitTest extends TestCase
+{
+    public function setUp()
+    {
+        $this->trait = $this->getMockForTrait(ComponentTreeTrait::class);
+
+        $this->comp1 = $this->getMockForAbstractClass(
+            AbstractComponent::class, ['comp1']
+        );
+        $this->comp2 = $this->getMockForAbstractClass(
+            AbstractComponent::class, ['comp2']
+        );
+        $this->comp3 = $this->getMockForAbstractClass(
+            AbstractComponent::class, ['comp3']
+        );
+        $this->comp4 = $this->getMockForAbstractClass(
+            AbstractComponent::class, ['comp4']
+        );
+        $this->comp5 = $this->getMockForAbstractClass(
+            AbstractComponent::class, ['comp5']
+        );
+    }
+
+    public function setComponents(array $components)
+    {
+        $closure = function () use ($components) {
+            $this->components = $components;
+        };
+
+        $closure->call($this->trait);
+    }
+
+    public function testTraverse1()
+    {
+        $this->setComponents([
+            'comp1' => $this->comp1,
+            'comp2' => $this->comp2,
+            'comp3' => $this->comp3,
+        ]);
+
+        $iter = $this->trait->traverse();
+
+        $this->assertEquals($this->comp1, $iter->current());
+        $iter->next();
+        $this->assertEquals($this->comp2, $iter->current());
+        $iter->next();
+        $this->assertEquals($this->comp3, $iter->current());
+    }
+
+    public function testTraverse2()
+    {
+        $this->comp1->addChild($this->comp2);
+        $this->comp2->addChild($this->comp3);
+        $this->comp3->addChild($this->comp4);
+        $this->comp4->addChild($this->comp5);
+
+        $this->setComponents([
+            'comp1' => $this->comp1,
+        ]);
+
+        $iter = $this->trait->traverse();
+
+        $this->assertEquals($this->comp1, $iter->current());
+        $iter->next();
+        $this->assertEquals($this->comp2, $iter->current());
+        $iter->next();
+        $this->assertEquals($this->comp3, $iter->current());
+        $iter->next();
+        $this->assertEquals($this->comp4, $iter->current());
+        $iter->next();
+        $this->assertEquals($this->comp5, $iter->current());
+    }
+
+    public function testTraverse3()
+    {
+        $this->comp1->addChild($this->comp3);
+        $this->comp1->addChild($this->comp4);
+        $this->comp2->addChild($this->comp5);
+
+        $this->setComponents([
+            'comp1' => $this->comp1,
+            'comp2' => $this->comp2,
+        ]);
+
+        $iter = $this->trait->traverse();
+
+        $this->assertEquals($this->comp1, $iter->current());
+        $iter->next();
+        $this->assertEquals($this->comp3, $iter->current());
+        $iter->next();
+        $this->assertEquals($this->comp4, $iter->current());
+        $iter->next();
+        $this->assertEquals($this->comp2, $iter->current());
+        $iter->next();
+        $this->assertEquals($this->comp5, $iter->current());
+    }
+}
